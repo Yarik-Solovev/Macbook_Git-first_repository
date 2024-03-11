413a1bfb5d8b12d3acda6d587594099231633959
Yarik-Solovev <yaroslav.solovev11@mail.ru>
2024-03-11 21:19:09 +0300

    опциональная цепочка

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
-console.log('heelo world');
+// const teslaCar = {
+//     name:'tesla',
+//     model:'X',
+//     // details:{
+//     //     color:'red',
+//     // }
+// }
+
+// const allCars = [teslaCar,]
+
+// let colorOfCar = ''
+
+// allCars.forEach(el => {
+//     colorOfCar += (el?.details?.color);//если элемента нет выдает  undefined
+// })
+
+// console.log(colorOfCar);
+
+
+
+// const b = 1;
+
+// let a = 0 ?? b;//задает значение если переменая не нашлась (??)
+// console.log(a);
