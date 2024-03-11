b36be4f4087bb071aad9fdde40dcb3b9ffb4ee91
Yarik-Solovev <yaroslav.solovev11@mail.ru>
2024-03-11 23:03:32 +0300

    деструктуризация

--- script.js
+++ script.js
@@ -22,3 +22,24 @@
 
 // let a = 0 ?? b;//задает значение если переменая не нашлась (??)
 // console.log(a);
+
+
+// const {name, surname, age} = {name:'Alex', surname:'Ivanov', age:52, };
+// console.log(name);//Alex
+// const [first, second, three, four, five,] = [1,2,3,4,5,]
+// console.log(five);
+
+// const [, , , , five,] = [1,2,3,4,5,]
+// console.log(five);
+
+// const [first, second, three, four, five = 0//я если элемента в массиве нет задается значение по умолчанию,] = [1,2,3,4,5,]
+// console.log(five);
+
+// const user = {
+//     name:'Alex',
+//     age:'23',
+// }
+
+// const {name: userName, age} = user;//name:... чтобы задать свое название ключу
+// console.log(userName);
+
