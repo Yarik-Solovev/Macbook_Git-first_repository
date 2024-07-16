8196da42261f08888a3743362d50c58f50727720
Yarik-Solovev <yaroslav.solovev11@mail.ru>
2024-07-16 14:46:05 +0300

    after work with useMemo

--- my-app/src/Hooks/Hooks.jsx
+++ my-app/src/Hooks/Hooks.jsx
@@ -1,7 +1,9 @@
-import { useState, useEffect, useRef } from 'react'
+import { useState, useEffect, useRef, useMemo } from 'react'
 
 export default function Hooks() {
 	const [value, setValue] = useState('')
+	const [number, setNumber] = useState(52)
+	const [color, setColor] = useState(false)
 	// useEffect(() => {
 	// 	console.log('Компонент отрисовался')//вызывается только один раз когда компонент отрисовался
 	// }, [])
@@ -9,12 +11,26 @@ export default function Hooks() {
 	// 	console.log('Компонент перерисовался')//вызывается когда компонент перерисововается
 	// })
 
-	const renderCount = useRef(1) //Когда состояние изменяется компонент не перерисовается
-	const input = useRef(null)
-	useEffect(() => {
-		renderCount.current++
-		console.log(renderCount.current, input.current.value)
+	// const renderCount = useRef(1) //Когда состояние изменяется компонент не перерисовается
+	// const input = useRef(null)
+	// useEffect(() => {
+	// 	renderCount.current++
+	// 	console.log(renderCount.current, input.current.value)
+	// })
+	const style = useMemo(() => {
+		return { color: color ? 'red' : 'black' }
 	})
+
+	function calculateRes(number) {
+		console.log('a') //при изменении цвета  log не вызывается из за useMemo
+		return (number * 1231) / 422
+	}
+
+	const res = useMemo(() => calculateRes(number), [number])
+
+	useEffect(() => {
+		console.log('Styled Changed') //при изменении number   не вызывается log из за useMemo
+	}, [style])
 
 	useEffect(() => {
 		console.log('Состояние изменилось')
@@ -23,13 +39,19 @@ export default function Hooks() {
 		<>
 			<h1>Value:{value} </h1>
 			<input
-				ref={input}
+				// ref={input}
 				type='text'
 				value={value}
 				onChange={e => {
 					setValue(e.target.value)
 				}}
 			/>
+			<p style={style}>{res}</p>
+			<button onClick={() => setNumber(current => ++current)}>увеличить</button>
+			<button onClick={() => setNumber(current => --current)}>уменьшить</button>
+			<button onClick={() => setColor(current => !current)}>
+				изменить цвет
+			</button>
 		</>
 	)
 }
